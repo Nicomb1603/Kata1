12114770862ca50bb13bcc18db244444bd28afa4
Nicolás Marcelo Batista <nicolasmb1603@gmail.com>
2024-10-11 12:39:39 +0100

    First Version

--- src/main/java/software/ulpgc/Person.java
+++ src/main/java/software/ulpgc/Person.java
@@ -4,8 +4,8 @@ import java.time.LocalDate;
 
 public class Person {
     private static final long DAYS_PER_YEAR = 365;
-    private String name;
-    private LocalDate birthday;
+    private final String name;
+    private final LocalDate birthday;
 
     public Person(String name, LocalDate birthday) {
         this.name = name;
