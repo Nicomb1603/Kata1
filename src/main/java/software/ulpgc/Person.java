c2dc8d90981362c026508b30082413590748c6f5
Nicolás Marcelo Batista <nicolasmb1603@gmail.com>
2024-10-11 12:38:52 +0100

    First Version

---
+++ src/main/java/software/ulpgc/Person.java
@@ -0,0 +1,39 @@
+package software.ulpgc;
+
+import java.time.LocalDate;
+
+public class Person {
+    private static final long DAYS_PER_YEAR = 365;
+    private String name;
+    private LocalDate birthday;
+
+    public Person(String name, LocalDate birthday) {
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", birthday=" + birthday +
+                ", age=" + getAge() +
+                '}';
+    }
+
+    private int getAge() {
+        return toYears(LocalDate.now().toEpochDay() - birthday.toEpochDay());
+    }
+
+    private int toYears(long days) {
+        return (int) (days/DAYS_PER_YEAR);
+    }
+}
